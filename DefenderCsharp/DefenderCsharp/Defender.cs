7ff9bc7c5357c0adbb4068658654f619077ff3c9
Unknown <overlordofeverything@gmail.com>
2018-02-17 14:53:53 -0800

    Made The C# file be able to input First and Last Name
    The First and Last Name Input Does not have any validations at this time, tester file was also made

---
+++ DefenderCsharp/DefenderCsharp/Defender.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace DefenderCsharp
+{
+    class Defender
+    {
+        public static void theDefender()
+        {
+            String first_name;
+            String last_name;
+
+            Boolean running = false;
+            do
+            {
+                first_name = getUserInput("Enter your First Name: ");
+                last_name = getUserInput("Enter your Last Name: ");
+                running = NameValidator(first_name, last_name);
+
+            } while (running);
+
+        }
+
+        private static Boolean NameValidator(String fistInput, String lastInput)
+        {
+            if (fistInput != null && lastInput != null)
+            {
+
+            }
+            return false;
+        }
+
+        //A basic Method that Requests input from user and will return it when input is given
+        private static String getUserInput(String typeofInput)
+        {
+            String input = null;
+
+            Console.Write(typeofInput);
+            input = Console.ReadLine();
+
+            return input;
+        }
+
+    }
+}
